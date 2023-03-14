2cf83cd13a7c1daa9eda146ede2e3cfa34aeeba4
Haeseok Kang <hotbloodturtle@gmail.com>
2023-03-14 16:17:24 +0900

    :sparkles: resolved #2 - add front, docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,16 +5,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { DataSource } from 'typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    // frontend static setting
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'frontend', 'dist'),
+      exclude: ['/api*'],
+    }),
+
+    // db setting
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DATABASE_HOST,
+      host: process.env.DATABASE_HOST || 'localhost',
       port: 3306,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USERNAME || 'root',
+      password: process.env.DATABASE_PASSWORD || '',
+      database: process.env.DATABASE_NAME || 'sixthree_moneybook_local',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false,
     }),
